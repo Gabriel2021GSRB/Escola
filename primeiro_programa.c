9818431976f7f65cabdf8e08cc6068bc4ee64b2d
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-02-01 11:46:00 -0300

    Create primeiro_programa.c
    Primeiro arquivo em c.

---
+++ primeiro_programa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+    float n1, n2, n3, n4, media;
+    printf("digite a primeira nota: ");
+    scanf("%f", &n1);
+    printf("digite a segunda nota: ");
+    scanf("%f", &n2);
+    printf("digite a terceira nota: ");
+    scanf("%f", &n3);
+    printf("digite a quarta nota: ");
+    scanf("%f", &n4);
+}
