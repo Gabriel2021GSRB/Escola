792d54461d8c6f1ad03dfac6f9948d158004e6d3
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-02-05 16:18:27 -0300

    Arquivo final.
    Este programa vai ser responsável por calcular as media escolares.

--- primeiro_programa.c
+++ primeiro_programa.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 void main(){
-    double n1, n2, n3, n4, media;
-    int contador, continua; 
-    printf("digite a primeira nota: ");
-    scanf("%f", &n1);
-    printf("digite a segunda nota: ");
-    scanf("%f", &n2);
-    printf("digite a terceira nota: ");
-    scanf("%f", &n3);
-    printf("digite a quarta nota: ");
-    scanf("%f", &n4);
-    media = n1+n2+n3+n4/4;
-    continua == 's';
-    contador =0;
+    float n1, n2, n3, n4, media;
+    int contador; 
+    char nome[30],continua; 
+    continua = 's';
     while ( continua == 's'){
+    printf("digitete o nome do aluno: \n");
+    scanf("%s", &nome);
+    printf("digite a primeira nota: \n");
+    scanf("%f", &n1);
+    printf("digite a segunda nota: \n");
+    scanf("%f", &n2);
+    printf("digite a terceira nota: \n");
+    scanf("%f", &n3);
+    printf("digite a quarta nota: \n");
+    scanf("%f", &n4);
+    media = (n1+n2+n3+n4)/4;
+    
     contador=contador+1;
-    printf(" a média do aluno é:", media);
+    printf(" a media do %s é: %.2f", nome,media);
     printf("Tecle 's' se deseja continuar\n");
+    scanf("%s",&continua);
+    
     
     }
+    return;
+    
 }
